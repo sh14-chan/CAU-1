ac9c231dbfd5be94225950a54636129d6ed19422
elizabeth <tdogb404@gmail.com>
2022-08-05 08:14:54 -0400

    the begnining but not really

---
+++ index.js
@@ -0,0 +1,18 @@
+const image = document.getElementById("poggy");
+const button = document.getElementById("theclicker");
+const loader = document.getElementById("loader");
+
+const url = "https://cataas.com/cat?json=true";
+
+async function onClick() {
+  loader.style.opacity = "1";
+  const res = await fetch(url);
+  const data = await res.json();
+  image.src = `https://cataas.com${data.url}`;
+}
+
+button.addEventListener("click", onClick);
+
+image.addEventListener("load", () => {
+  loader.style.opacity = "0";
+});
